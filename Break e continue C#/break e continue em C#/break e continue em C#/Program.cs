7b989a6f3810f3b44f8a5a866d605be46a92a7e8
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-12 15:21:56 -0300

    Laços de repetição break e continue em C#

---
+++ Break e continue C#/break e continue em C#/break e continue em C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_e_continue_em_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Aperte C para utilizar 'continue'.");
+                Console.WriteLine("Aperte B para utilizar 'break'.");
+                string tecla = Console.ReadKey(true).KeyChar.ToString();
+                
+
+                if (tecla == "c")
+                {
+                    continue;
+
+                }
+                else if (tecla == "b")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Esta tecla é desconhecida");
+
+                }
+                
+            }
+            Console.WriteLine("Pressione qualquer tecla para sair");
+             Console.ReadKey();
+        }
+    }
+}
