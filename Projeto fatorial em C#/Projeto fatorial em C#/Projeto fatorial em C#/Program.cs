75ff5b045b28b31e4a9d0af22c0eff89fba25ac1
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-15 11:21:24 -0300

    Projeto fatorial em C#
    fiz um projeto que realiza a fatoração de números

---
+++ Projeto fatorial em C#/Projeto fatorial em C#/Projeto fatorial em C#/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_fatorial_em_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine(" Pressione qualquer número ou pressione b para sair");
+                string botao = Console.ReadLine();
+
+                if (botao == "b" || botao == "B")
+                    break;              
+                else
+                {
+                    int numero = Convert.ToInt32(botao);
+                    if (numero == 0)                    
+                        Console.WriteLine("Não é possível fazer uma fatorial com números negativos.");                   
+                    else if (numero < 0)
+                    Console.WriteLine("0! = 1");
+                    else
+                    {
+                        int numeroFatorado = 1;
+                        Console.Write(numero + "! = ");
+                        for (int fatores = numero; fatores > 0; fatores--)
+                        {
+                            numeroFatorado *= fatores;
+                            if (fatores == 1)
+                            Console.Write(fatores + " = " + numeroFatorado);
+                            else
+                            {
+                                Console.Write(fatores + " x");
+                            }
+                        }
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
