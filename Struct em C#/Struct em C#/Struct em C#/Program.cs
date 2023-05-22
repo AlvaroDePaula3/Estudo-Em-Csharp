7d053a580f915db045e86a6a94c9a1c76ba3dd93
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-22 11:43:50 -0300

    Struct em C#

---
+++ Struct em C#/Struct em C#/Struct em C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_em_C_
+{
+    internal class Program
+    {
+        struct Musica{
+            public string nome;
+            public string album;
+            public DateTime dataDeLancamento;
+            public string Artista;
+        }
+        static void Main(string[] args)
+        {
+            Musica song;
+
+            song.nome = "The pretender";
+            song.album = "Echoes, Silence, Patience & Grace";
+            song.dataDeLancamento = Convert.ToDateTime("20/02/2002");
+            song.Artista = "Foo fighters";
+
+            Console.WriteLine(song.nome);
+            Console.WriteLine(song.album);
+            Console.WriteLine(song.dataDeLancamento);
+            Console.WriteLine(song.Artista);
+
+            Console.ReadKey();
+        }
+    }
+}
