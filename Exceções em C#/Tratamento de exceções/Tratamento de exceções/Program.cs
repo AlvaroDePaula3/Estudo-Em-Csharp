43546159a8423c0c475e002faa4e06e42cdb901a
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-25 23:42:12 -0300

    Exceções em C#

---
+++ Exceções em C#/Tratamento de exceções/Tratamento de exceções/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratamento_de_exceções
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem vindo, digite um número");
+
+            try
+            {
+            int dividir = Convert.ToInt32(Console.ReadLine());
+            int divisao = 10 / dividir;
+            Console.WriteLine(divisao);
+
+            }catch (DivideByZeroException zerada)
+            {
+                Console.WriteLine(zerada.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Pressione qualquer tecla para fechar a aplicação");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
