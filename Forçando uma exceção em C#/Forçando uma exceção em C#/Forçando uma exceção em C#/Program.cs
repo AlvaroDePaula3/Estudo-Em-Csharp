24f80e8f0daaf1c4424759e630319090502e1039
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-05-26 13:20:00 -0300

    Forçando uma exceção em C#

---
+++ Forçando uma exceção em C#/Forçando uma exceção em C#/Forçando uma exceção em C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forçando_uma_exceção_em_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Digite um número acima de 20");
+            int algarismo = Convert.ToInt32(Console.ReadLine());
+            if (algarismo < 20) {
+                    throw new Exception("O número " + algarismo + " é abaixo de 20");
+                } else
+                {
+                    Console.WriteLine("o número " + algarismo + " é maior que 20");
+                }
+            }
+            catch (Exception excecao )
+            {
+                Console.WriteLine("exceção: " + excecao.Message);
+            }
+            finally
+            {
+            Console.WriteLine("Aperte qualquer tecla pra sair");
+            Console.ReadKey();
+
+            }
+
+        }
+    }
+}
