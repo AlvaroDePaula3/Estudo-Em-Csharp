1729605aec85d690beb88dede3c03a81a12933bd
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2023-06-09 02:49:22 -0300

    Manipulação de arquivos 3
    Finalização do módulo básico do curso de C#

---
+++ Manipulação de arquivos 3 em C#/Manipulação de arquivos 3 em C#/Manipulação de arquivos 3 em C#/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Manipulação_de_arquivos_3_em_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = "Manipulacao-De-Arquivos3.txt";
+            FileStream arquivo = File.Create(caminho);
+            arquivo.Close();
+
+            StreamWriter escrever = new StreamWriter(caminho);
+            escrever.Write("a");
+            escrever.Write(" - ");
+            escrever.Write("Olá ");
+            escrever.Write("Mundo! \r\n");
+            escrever.WriteLine("Hello World");
+            escrever.WriteLine("Kon'nichiwa sekai");
+            escrever.WriteLine("Hola mundo");
+            escrever.WriteLine("Bonjour le monde");
+
+            escrever.Close();
+
+            //Podemos utilizar também o escrever.Write, porém ele não pula linhas, apenas se vc colocar o \r\n no final 
+
+            StreamReader ler = new StreamReader(caminho);
+
+            char[] buffer = new char[128];
+            ler.Read(buffer, 3, 5);
+            ler.Close();
+
+            //ler linhas 
+
+            StreamReader ler2 = new StreamReader(caminho);
+            string lerLinhas = ler2.ReadLine();
+            lerLinhas = ler2.ReadLine();
+            ler2.Close();
+
+            //ler até o final
+
+            StreamReader ler3 = new StreamReader(caminho);
+            string lerTudo = ler3.ReadToEnd();
+            ler3.Close();
+        }
+    }
+}
