23787533a8830ab92c49368d9d07c25702b5147c
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-06-27 14:25:45 -0300

    Termômetro em C#
    Código que mostra no console a temperatura colocada pelo usuário e mostra erro caso tenha colocado temperatura abaixo de 273.15 (pois nesse caso seria 0 absoluto)

---
+++ termometro/termometro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Termometro
+{
+    class Program
+    {
+        class termometro
+        {
+            private double temperatura;
+
+            public double Temp
+            {
+                get { return temperatura; }
+                set
+                {
+                    if (temperatura < -273.15)
+                    {
+                        Console.WriteLine("A temperatura não pode ser abaixo do 0 absoluto.");
+                    }
+                    else
+                    {
+                        temperatura = value;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            termometro Termo = new termometro();
+            Console.WriteLine("Digite a temperatura atual");
+
+            double valor = double.Parse(Console.ReadLine());
+            Termo.Temp = valor;
+            Console.WriteLine($"Temperatura atual é: {valor}ºC");
+
+            Console.ReadKey();
+        }
+    }
+}
