194c25954961a0eacc0e98b27798c20ee2c80540
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-07-06 23:42:51 -0300

    Exercícios Delegate
    Exercícios para fixação de delegates em C#

---
+++ Exercícios Delegate/Exercícios Delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercícios_Delegate
+{
+    internal class Program
+    {
+        public delegate double Calculo(double x, double y);
+
+        public double Somar(double x, double y) => x + y;
+
+        public double Multiplicar(double x, double y) => x * y;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercício Func");
+            Func<double, double> dobro = x => x * 2;
+
+            double resultado = dobro(5.5);
+
+            Console.WriteLine($"{resultado}");
+
+            Console.WriteLine();
+            Console.WriteLine("Exercício Action");
+
+            Action<string> cumprimentos = nomear => Console.WriteLine($"Opa {nomear} ta deboas ?");
+           cumprimentos("João");
+
+            Console.WriteLine();
+
+            Program p = new Program();
+
+            Calculo operacao = p.Somar;
+            Console.WriteLine(operacao(5, 5));
+
+            operacao = p.Multiplicar;
+            Console.WriteLine(operacao(2, 5));
+
+            Console.WriteLine();
+
+        }
+    }
+}
