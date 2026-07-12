a117a4c15db1cdfdf5af387e7499947af8c3df68
Alvaro De Paula <114239272+AlvaroDePaula3@users.noreply.github.com>
2026-07-11 23:45:22 -0300

    Treinamento com Tasks em c#
    Treinamento com Tasks em c#

---
+++ treinamentoTask/treinamentoTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Threading.Tasks;
+
+namespace treinamentoTask
+{
+    internal class Program
+    {
+        public async Task<string> esperando3SegundosAsync()
+        {
+            await Task.Delay(3000);
+            return "Esperei 3 segundos !";
+        }
+        static async Task Main(string[] args)
+        {
+            var esperado = await new Program().esperando3SegundosAsync();
+            Console.WriteLine(esperado);
+        }
+    }
+}
